cd074cae85e51ebbe60dd868c3afbdc2a1de8c43
Jakob F <Jakobfoersom88@gmail.com>
2023-10-16 09:11:35 +0200

    Billeder virker
    Jeg har tilføjet kommentar til min javaScript kode og fiksede billederne så de rigtige billede er ved de rigtige karaktere også når man skifter side.

--- js-RickAndMorty.js
+++ js-RickAndMorty.js
@@ -1,4 +1,5 @@
-let number = 0;
+let number = 1;
+let imageNumber = 1;
 
 async function populate(int) {
   const requestURL =
@@ -7,62 +8,82 @@ async function populate(int) {
 
   const response = await fetch(request);
   const rickOgMortyPersoner = await response.json();
+  
+  
+  udfyldPersonInfo(rickOgMortyPersoner,20);//Jeg kalder funktionen og indsætter 20 fordi hvor side af
+  //character?page indeholder 20 personer.
 
-  udfyldPersonInfo(rickOgMortyPersoner,20);
+
+
 }
 
 function udfyldPersonInfo(obj,int){
 
   for (let i = 0; i < int; i++){ 
-
-  const header = document.querySelector("header");
-  const container = document.createElement("div");
+  
+  const header = document.querySelector("header");//Jeg finder html elementet header
+  const container = document.createElement("div");// jeg opretter en div container til at samle oplysninger om karakterne i.
 
   const navn = document.createElement("h1");
   const gender = document.createElement("p");
   const status = document.createElement("p");
   const origin = document.createElement("p");
+  const image = document.createElement("img");
 
+  //Jeg trækker navn,køn,status og origin ud af objeketet.
   navn.textContent = obj.results[i].name;
   gender.textContent = `Gender : ${obj.results[i].gender}`;
   status.textContent = `Status : ${obj.results[i].status}`;
   origin.textContent = `Origin : ${obj.results[i].origin.name}`;
+  //Jeg henter billede fra nedestående link "i" og imageNumber sørger for at de rigtige billeder 
+  //bliver sat sammen med de rigtige karaktere.  
+  image.src = 	`https://rickandmortyapi.com/api/character/avatar/${i + imageNumber}.jpeg`
 
-  //her tilføjes CSS
+  //her tilføjes CSS linket til rickAndMortysStyle
   container.classList.add("box-style");
+  image.classList.add("character-image");
 
+  //Her indsætter jeg navn,køn,status, origin og billede i containeren.
   container.appendChild(navn);
+  container.appendChild(image);
   container.appendChild(gender);
   container.appendChild(status);
   container.appendChild(origin);
-
   header.appendChild(container);
   }
 }
+//Her henter jeg nextButton og previousButton knapperne og tilføjer eventListener.
 const nextPageButton = document.getElementById("next-button");
 nextPageButton.addEventListener("click",nextPage);
 
 const previousPageButton = document.getElementById("previous-button");
 previousPageButton.addEventListener("click",previousPage);
 
+
+//Disse funktioner sørger for man kan skifte siderne
+
 function nextPage(){
+  
   if(number < 42){
     number++;
+    imageNumber += 20;
   }else{
     number = 42;
   }
     const header = document.querySelector("header");
     header.innerHTML = "";
-    populate(number);
+    
   }
+
+
 function previousPage(){
   if(number > 1){
     number--;
+    imageNumber -= 20;
   }else{
     number = 1;
   }
   const header = document.querySelector("header");
     header.innerHTML = "";
-    populate(number);
 }
-nextPage();
+populate(number);//Her kalder jeg populate funktionen så siden starter.
